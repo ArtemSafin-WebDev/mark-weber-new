0b7f584a7a198036a72caadd4ecd26cbd21b640f
Artem Safin <artemsafin.webdev@gmail.com>
2020-12-16 20:36:31 +0300

    Добавлены якорные ссылки

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,13 @@ import polyfills from './polyfills';
 import detectTouch from './detectTouch';
 import MainSlider from './mainSliderNew';
 import IntroSlider from './introSlider';
+import AnchorLinks from './anchorLinks';
 
 document.addEventListener('DOMContentLoaded', function() {
     polyfills();
     detectTouch();
     IntroSlider();
+    AnchorLinks();
     window.mainSlider = new MainSlider(document.querySelector('.js-main-slider'));
 
 
